0c4c50ffc60c3d5c23834cb460fa9b6f658eb1ec
cyjlnk <2743319061@qq.com>
2021-05-31 00:02:07 +0800

    任务二 AddFound页面后退键测试成功

--- latte-ec/src/main/java/com/flj/latte/ec/main/found/AddFoundDelegate.java
+++ latte-ec/src/main/java/com/flj/latte/ec/main/found/AddFoundDelegate.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.diabin.latte.ec.R;
 import com.flj.latte.delegates.bottom.BottomItemDelegate;
@@ -17,7 +18,8 @@ import me.yokeyword.fragmentation.anim.FragmentAnimator;
 
 public class AddFoundDelegate extends BottomItemDelegate {
 
-    
+    private ImageView iv_found_back,iv_found_add;
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_add_found;
@@ -30,7 +32,21 @@ public class AddFoundDelegate extends BottomItemDelegate {
     @Override
     public void onLazyInitView(@Nullable Bundle savedInstanceState) {
         super.onLazyInitView(savedInstanceState);
+        iv_found_back=$(R.id.iv_found_back);
+        iv_found_add=$(R.id.iv_found_add);
+        iv_found_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    getSupportDelegate().start(new FoundDelegate());
+            }
+        });
 
+        iv_found_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportDelegate().start(new FoundDelegate());
+            }
+        });
     }
 
     @Override
